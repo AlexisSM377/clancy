6b7428f40e9f97d4df95747f586dadd24403b702
AlexisSM377 <al222111377@gmail.com>
2025-01-06 23:02:37 -0600

    add: create principal date to componen

--- src/app/sections/hero.tsx
+++ src/app/sections/hero.tsx
@@ -1,9 +1,11 @@
+import { HeroLogo } from "../components/icons/herologo"
+import { PrincipalDate } from "../components/PrincipalDate"
+
 export const Hero = () => {
     return (
-        <div>
-            <h1>
-                Hero
-            </h1>
-        </div>
+        <section className="flex flex-col justify-center items-center pt-40 gap-9">
+            <HeroLogo className="w-full h-auto animate-blurred-fade-in" />
+            <PrincipalDate />
+        </section>
     )
 }
